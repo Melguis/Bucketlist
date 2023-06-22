d6112a695c2bb971a813ff8fb559200eccc94986
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-22 19:15:36 -0300

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Jorge Henrique on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
